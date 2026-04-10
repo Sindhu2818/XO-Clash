d76fa26d0decf976c9594f4df1ef6a9a848b7b00
Sindhu <raga.yarlagadda@students.iiit.ac.in>
2026-04-10 22:23:07 +0530

    Updated db_mongo.py

--- db_mongo.py
+++ db_mongo.py
@@ -1,24 +1,18 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017/")
-db = client["arena_db"]
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def get_db():
-<<<<<<< HEAD
-    return db
-
-def upsert_image(db, uid: str, image_data: str):
-    result = db.profile_images.update_one(
-=======
     client = MongoClient(os.getenv("MONGO_URI"))
     return client["arena_db"]
 
 
 def upsert_image(db, uid: str, image_data: str):
     db.profile_images.update_one(
->>>>>>> df19400 (Updated files)
         {"uid": uid},
         {"$set": {"image": image_data}},
         upsert=True
     )
-    print(f"[Mongo] Insert {uid}: {result.acknowledged}")
